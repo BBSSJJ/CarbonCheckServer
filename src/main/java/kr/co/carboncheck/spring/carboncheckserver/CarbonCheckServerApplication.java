a1fc09093e1131eef4c701067591ec5c39b8c766
BBSSJJ <bdhbsj@naver.com>
2023-04-07 20:20:50 +0900

    initial setting

---
+++ src/main/java/kr/co/carboncheck/spring/carboncheckserver/CarbonCheckServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.carboncheck.spring.carboncheckserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonCheckServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonCheckServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/carboncheck/spring/carboncheckserver/CarbonCheckServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.carboncheck.spring.carboncheckserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonCheckServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
