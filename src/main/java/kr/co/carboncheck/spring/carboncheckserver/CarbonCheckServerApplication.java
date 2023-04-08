482a9ebed43e6f60d00235ff2dc6b1eeb008394c
BBSSJJ <bdhbsj@naver.com>
2023-04-08 15:56:55 +0900

    안드로이드와 통신 성공

--- src/main/java/kr/co/carboncheck/spring/carboncheckserver/CarbonCheckServerApplication.java
+++ src/main/java/kr/co/carboncheck/spring/carboncheckserver/CarbonCheckServerApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarbonCheckServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarbonCheckServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonCheckServerApplication.class, args);
+        System.out.println("시작!");
+    }
 
 }
